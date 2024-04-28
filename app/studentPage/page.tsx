d6d03c7e82ea49a75b154fdab5b094e1681b0550
lowerzedo <tp052475@mail.apu.edu.my>
2024-04-28 16:21:10 +0800

    fix: styling

--- app/studentPage/page.tsx
+++ app/studentPage/page.tsx
@@ -354,7 +354,7 @@ export default function StudentPage() {
                   onChange={(e) => {
                     setSelectedAssessment(e.target.value);
                   }}
-                  className="mb-4 p-2"
+                  className="py-3 px-4 inline-flex items-center gap-x-2 text-2xl font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                 >
                   <option value="" disabled>
                     Select Assessment
